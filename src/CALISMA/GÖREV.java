e8e40179366e75d2ad8457b2869712f408ab07b6
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-31 11:36:50 +0200

    ortak

---
+++ src/CALISMA/GÖREV.java
@@ -0,0 +1,41 @@
+package CALISMA;
+
+public class GÖREV {
+
+    public static void main(String[] args) {
+
+
+        /* Ögrenci EKLEME  ======>>FATMA TASDEMIR
+
+        1.
+        private String ogrenciNo;
+        private String sinif;
+
+    public Ogrenci(String adSoyad, String kimlikNo, int yas, String ogrenciNo, String sinif) {
+            super(adSoyad, kimlikNo, yas);
+            this.ogrenciNo = ogrenciNo;
+            this.sinif = sinif;
+
+
+        }
+    }
+    */
+
+
+        /* ÖGRETMEN EKLEME ======>>> MIHRICAN
+     public Ogretmen(){
+        }
+
+    public Ogretmen(String adSoyad, String kimlikNo, int yas, String bolum, String sicilNo) {
+            super(adSoyad, kimlikNo, yas);
+            this.bolum = bolum;
+            this.sicilNo = sicilNo;
+
+
+        }
+
+
+*/
+
+    }
+}
